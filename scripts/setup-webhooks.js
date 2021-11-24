bbb03bb5e10274513b6f3fc99742e3e90fcaf1ee
Giorgio Garasto <giorgio@garasto.me>
2021-11-24 19:21:13 +0100

    feat: implement script to automatically setup webhooks on deploy

---
+++ scripts/setup-webhooks.js
@@ -0,0 +1,28 @@
+import childProcess from 'child_process';
+import fetch from 'node-fetch';
+
+const setWebhook = async url => {
+  const token = url.slice(url.indexOf('bot') + 3);
+  const response = await fetch(
+    `https://api.telegram.org/bot${token}/setWebhook`,
+    {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ url }),
+    },
+  );
+  const { ok, description } = await response.json();
+
+  if (response.status !== 200 || !ok) {
+    throw new Error(
+      `Couldn't set webhook for bot with token "${token}"${
+        description ? `: ${description}` : ''
+      }`,
+    );
+  }
+};
+
+childProcess.exec('serverless info', async (_, stdout) => {
+  const urls = Array.from(stdout.matchAll(/POST - (.+)/g));
+  await Promise.all(urls.map(([_, match]) => setWebhook(match)));
+});
