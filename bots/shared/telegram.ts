93cf261221976c247ac11c5c820746db5322e02b
Giorgio Garasto <giorgio@garasto.me>
2021-11-29 08:11:40 +0100

    feat: implement shared helpers to get allowed chat ids and bot updates

--- bots/shared/telegram.ts
+++ bots/shared/telegram.ts
@@ -1,4 +1,4 @@
-import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot, { Update } from 'node-telegram-bot-api';
 
 let bot: TelegramBot;
 
@@ -26,3 +26,71 @@ export const getBotUsername = async (bot: TelegramBot) => {
 
 export const getBotStartRegex = (username: string) =>
   new RegExp(`^\\/start(?:@${username})?\s*$`);
+
+export const getAllowedChatIds = (rawIds = process.env.CHAT_IDS_ALLOWLIST) => {
+  if (!rawIds) {
+    throw new Error('Chat ids allowlist not provided!');
+  }
+
+  return (
+    rawIds
+      // Split by comma
+      .split(',')
+      // Trim whitespaces and convert to number
+      // If the string is empty, convert it to a string that can't be
+      // casted to a number so that it gets casted to NaN instead of 0
+      .map(id => Number(id.trim() || 'NaN'))
+      // Filter out NaNs
+      .filter(id => !isNaN(id))
+  );
+};
+
+const extractChatId = (update: Update): number | undefined =>
+  update.message?.chat.id ||
+  update.callback_query?.message?.chat.id ||
+  update.channel_post?.chat.id ||
+  update.edited_channel_post?.chat.id;
+
+export interface GetAllUpdatesResult {
+  updates: Record<string, Update[]>;
+  lastUpdateId: number;
+}
+
+export const getAllUpdates = async (
+  bot: TelegramBot,
+  offset: number = 0,
+  currentUpdatesResult?: GetAllUpdatesResult,
+): Promise<GetAllUpdatesResult> => {
+  const newUpdates = await bot.getUpdates({
+    allowed_updates: ['message'],
+    offset: offset + 1,
+  });
+
+  if (newUpdates.length < 1) {
+    return (
+      currentUpdatesResult || {
+        updates: {},
+        lastUpdateId: offset,
+      }
+    );
+  }
+
+  const latestOffset = newUpdates.reduce(
+    (highest, { update_id }) => Math.max(highest, update_id),
+    0,
+  );
+
+  return getAllUpdates(bot, latestOffset, {
+    updates: newUpdates.reduce((acc, update) => {
+      const chatId = extractChatId(update);
+
+      return chatId
+        ? {
+            ...acc,
+            [`${chatId}`]: [...(acc[chatId] || []), update],
+          }
+        : acc;
+    }, currentUpdatesResult?.updates || {}),
+    lastUpdateId: latestOffset,
+  });
+};
