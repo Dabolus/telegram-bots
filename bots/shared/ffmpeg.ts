0776a6ea8ec033aaa7d0e6ec5f13dbb6a61b71c3
Giorgio Garasto <giorgio@garasto.me>
2022-01-13 19:29:02 +0100

    feat: implement helper to get file info using ffprobe

--- bots/shared/ffmpeg.ts
+++ bots/shared/ffmpeg.ts
@@ -2,8 +2,22 @@ import cp from 'child_process';
 import util from 'util';
 
 const ffmpegPath = '/opt/bin/ffmpeg';
+const ffprobePath = '/opt/bin/ffprobe';
 
 const run = util.promisify(cp.exec);
 
 export const runFfmpeg = (params?: string) =>
   run(`${ffmpegPath} ${params || ''}`);
+
+export const runFfprobe = (params?: string) =>
+  run(`${ffprobePath} ${params || ''}`);
+
+export const getFileInfo = async (filePath: string) => {
+  const { stdout } = await runFfprobe(`-i ${filePath} -show_streams`);
+  return Object.fromEntries(
+    Array.from(stdout.matchAll(/^([\w_]+)=(.+)$/gm)).map(([, key, val]) => {
+      const numericVal = Number(val);
+      return [key, isNaN(numericVal) ? val : numericVal];
+    }),
+  );
+};
