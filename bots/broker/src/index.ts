0a49c8ea964dfed2aaa5425a1d4a9c8be26bbc8c
Giorgio Garasto <giorgio@garasto.me>
2024-03-14 21:35:13 +0100

    feat(broker): add support for analytics

--- bots/broker/src/index.ts
+++ bots/broker/src/index.ts
@@ -1,11 +1,16 @@
 import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
-import type { Update } from 'node-telegram-bot-api';
+import type {
+  Update,
+  Message,
+  PollAnswer,
+  CallbackQuery,
+} from 'node-telegram-bot-api';
 import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent, ctx: Context) => {
   ctx.callbackWaitsForEmptyEventLoop = false;
 
-  console.info('Received event', event);
+  console.info('Received event', event.requestContext);
 
   if (!event.body) {
     console.error('No body provided');
@@ -56,4 +61,64 @@ export const handler = async (event: APIGatewayProxyEvent, ctx: Context) => {
       },
     }),
   );
+
+  if (process.env.ENABLE_ANALYTICS) {
+    try {
+      console.info('Sending analytics event');
+      const update: Update = JSON.parse(event.body);
+      const updateType = Object.keys(update).find(
+        key => key !== 'update_id',
+      ) as Exclude<keyof Update, 'update_id'>;
+      const updateObject = update[updateType];
+      const updateUnixTime =
+        (updateObject as Message)?.edit_date ??
+        (updateObject as Message)?.date ??
+        Math.round(Date.now() / 1000);
+      const chat =
+        (updateObject as Message)?.chat ??
+        (updateObject as CallbackQuery)?.message?.chat;
+      const user =
+        (updateObject as Message)?.from ?? (updateObject as PollAnswer)?.user;
+      const clientId = [chat?.id ?? 0, user?.id ?? 0].join('.');
+      const res = await fetch(
+        `https://www.google-analytics.com/mp/collect?${new URLSearchParams({
+          api_secret: process.env.ANALYTICS_API_SECRET!,
+          measurement_id: process.env.ANALYTICS_MEASUREMENT_ID!,
+        }).toString()}`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            client_id: clientId,
+            timestamp_micros: updateUnixTime * 1000000,
+            ...(user && {
+              user_id: user.id.toString(),
+              user_properties: {
+                language_code: { value: user.language_code },
+                is_bot: { value: !!user.is_bot },
+                // @ts-expect-error is_premium field is missing from typings, but it exists
+                is_premium: { value: !!user.is_premium },
+              },
+            }),
+            events: [
+              {
+                name: updateType,
+                params: {
+                  bot: event.pathParameters.botId,
+                  user_id: user?.id,
+                  chat_id: chat?.id,
+                  chat_type: chat?.type,
+                  is_forum:
+                    chat?.type === 'supergroup' ? !!chat.is_forum : undefined,
+                },
+              },
+            ],
+          }),
+        },
+      );
+      console.info(`Analytics response: ${res.status} ${res.statusText}`);
+    } catch (error) {
+      console.error('Error while sending analytics event', error);
+    }
+  }
 };
